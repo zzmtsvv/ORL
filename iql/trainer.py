c5e71a78b7c4e0752eb7be0acd386d154d86b919
Alexey Zemtsov <zzemtsovv@yandex.ru>
2023-08-23 07:18:13 +0300

    Create trainer.py

---
+++ iql/trainer.py
@@ -0,0 +1,44 @@
+import torch
+from iql import IQL
+from config import iql_config
+from modules import Actor, EnsembledCritic
+from dataset import ReplayBuffer
+
+import wandb
+from tqdm import tqdm
+
+
+class IQLTrainer:
+    def __init__(self,
+                 cfg=iql_config) -> None:
+        self.cfg = cfg
+        self.device = cfg.device
+
+        self.state_dim = 17
+        self.action_dim = 6
+        self.batch_size = cfg.batch_size
+
+        self.iql = IQL(cfg)
+        
+        self.buffer = ReplayBuffer(self.state_dim, self.action_dim, cfg.buffer_size)
+        self.buffer.from_json(cfg.dataset_name)
+    
+    def fit(self):
+        print(f"Training starts on {self.cfg.device} 🚀")
+
+        with wandb.init(project=self.cfg.project, entity="zzmtsvv", group=self.cfg.group, name=self.cfg.name):
+            wandb.config.update({k: v for k, v in self.cfg.__dict__.items() if not k.startswith("__")})
+
+            for t in tqdm(range(self.cfg.max_timesteps), desc="IQL steps"):
+                
+                batch = self.buffer.sample(self.batch_size)
+
+                states, actions, rewards, next_states, dones = [x.to(self.device) for x in batch]
+
+                logging_dict = self.iql.train(states,
+                                                 actions,
+                                                 rewards,
+                                                 next_states,
+                                                 dones)
+                
+                wandb.log(logging_dict, step=self.iql.total_iterations)
